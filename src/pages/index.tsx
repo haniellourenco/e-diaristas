e6197f5d65bd4238cb4fc76051e9f701ea489c89
Haniel <hanielohn2016@gmail.com>
2021-10-16 00:03:38 -0300

    aula01 finalizada

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,33 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironment";
+import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
+import UserInformation from "ui/components/data-display/UserInformation/UserInformation";
 
 const Home: NextPage = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+	return (
+		<div>
+			<SafeEnvironment />
+			<PageTitle
+				title={"Conheça os profissionais"}
+				subtitle={
+					"Preencha seu endereço e veja todos os profissionais da sua localidade"
+				}
+			/>
 
-export default Home
+			<UserInformation
+				name={"Haniel Lourenço"}
+				picture={"https://github.com/haniellourenco.png"}
+				rating={4}
+				description={"Joinville"}
+			/>
+			<UserInformation
+				name={"Eduarda Rech"}
+				picture={""}
+				rating={5}
+				description={"Joinville"}
+			/>
+		</div>
+	);
+};
+
+export default Home;
