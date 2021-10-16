c59b980aeb833f1a1d347b6c4e62727e0b7f1cd4
Haniel <hanielohn2016@gmail.com>
2021-10-16 15:20:04 -0300

    tela pronta

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,13 @@ import type { NextPage } from "next";
 import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironment";
 import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
 import UserInformation from "ui/components/data-display/UserInformation/UserInformation";
+import TextFieldMask from "ui/components/inputs/TextFieldMask/TextFieldMask";
+import { Button, Typography, Container } from "@mui/material";
+import {
+	FormElementsContainer,
+	ProfissionaisContainer,
+	ProfissionaisPaper,
+} from "ui/styles/pages/index.style";
 
 const Home: NextPage = () => {
 	return (
@@ -13,19 +20,65 @@ const Home: NextPage = () => {
 					"Preencha seu endereço e veja todos os profissionais da sua localidade"
 				}
 			/>
+			<Container>
+				<FormElementsContainer>
+					<TextFieldMask
+						mask={"99.999-999"}
+						label={"Digite seu CEP"}
+						fullWidth
+						variant={"outlined"}
+					/>
 
-			<UserInformation
-				name={"Haniel Lourenço"}
-				picture={"https://github.com/haniellourenco.png"}
-				rating={4}
-				description={"Joinville"}
-			/>
-			<UserInformation
-				name={"Eduarda Rech"}
-				picture={""}
-				rating={5}
-				description={"Joinville"}
-			/>
+					<Typography color={"error"}>CEP inválido</Typography>
+					<Button
+						variant={"contained"}
+						color={"secondary"}
+						sx={{ width: "220px" }}
+					>
+						Buscar
+					</Button>
+				</FormElementsContainer>
+				<ProfissionaisPaper>
+					<ProfissionaisContainer>
+						<UserInformation
+							name={"Haniel Lourenço"}
+							picture={"https://github.com/haniellourenco.png"}
+							rating={4}
+							description={"Joinville"}
+						/>
+						<UserInformation
+							name={"Eduarda Rech"}
+							picture={""}
+							rating={5}
+							description={"Joinville"}
+						/>
+						<UserInformation
+							name={"Haniel Lourenço"}
+							picture={"https://github.com/haniellourenco.png"}
+							rating={4}
+							description={"Joinville"}
+						/>
+						<UserInformation
+							name={"Eduarda Rech"}
+							picture={""}
+							rating={5}
+							description={"Joinville"}
+						/>
+						<UserInformation
+							name={"Haniel Lourenço"}
+							picture={"https://github.com/haniellourenco.png"}
+							rating={4}
+							description={"Joinville"}
+						/>
+						<UserInformation
+							name={"Eduarda Rech"}
+							picture={""}
+							rating={5}
+							description={"Joinville"}
+						/>
+					</ProfissionaisContainer>
+				</ProfissionaisPaper>
+			</Container>
 		</div>
 	);
 };
